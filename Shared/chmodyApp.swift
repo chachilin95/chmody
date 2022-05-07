cb587e23ca413c1b75387fe5329e341e44547efd
Alejandro Figueroa <chachilin95@me.com>
2022-05-07 09:21:28 -0500

    Initial Commit

---
+++ Shared/chmodyApp.swift
@@ -0,0 +1,17 @@
+//
+//  chmodyApp.swift
+//  Shared
+//
+//  Created by Alejandro Figueroa on 5/7/22.
+//
+
+import SwiftUI
+
+@main
+struct chmodyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

9194a9900bac9e5882bd6c34d8b49d97bcf7c170
Alejandro Figueroa <chachilin95@me.com>
2022-05-11 22:09:28 -0500

    More work

--- Shared/ContentView.swift
+++ Shared/ContentView.swift
@@ -22,30 +22,31 @@ struct UserClass {
     }
 }
 
-func makeOctal(_ o: Settings, _ g: Settings, _ e: Settings) -> String {
-    return [o,g,e].reduce("") {
-        $0 + String(($1.read ? 4 : 0) + ($1.write ? 2 : 0) + ($1.execute ? 1 : 0))
+struct UserClassSettingView: View {
+    var label: String
+    @Binding var val: Bool
+    
+    var body: some View {
+        Toggle(isOn: $val) {
+            Text(label)
+                .scaledToFill()
+                .frame(maxWidth: .infinity)
+        }.toggleStyle(.button)
     }
 }
-
-func makeSymbol(_ o: Settings, _ g: Settings, _ e: Settings) -> String {
-    return [o,g,e].reduce("") {
-        $0 + ($1.read ? "r" : "-") + ($1.write ? "w" : "-") + ($1.execute ? "x" : "-")
-    }
-}
-
 
 struct UserClassView: View {
     @Binding var userClass: UserClass
     
     var body: some View {
-        VStack {
+        VStack() {
             Text(userClass.title)
                 .font(.title)
+                .multilineTextAlignment(.leading)
             VStack {
-                Toggle("Read", isOn: $userClass.settings.read)
-                Toggle("Write", isOn: $userClass.settings.write)
-                Toggle("Execute", isOn: $userClass.settings.execute)
+                UserClassSettingView(label: "Read", val: $userClass.settings.read)
+                UserClassSettingView(label: "Write", val: $userClass.settings.write)
+                UserClassSettingView(label: "Execute", val: $userClass.settings.execute)
             }
         }
     }
@@ -54,21 +55,49 @@ struct UserClassView: View {
 struct ContentView: View {
     @State private var owner = UserClass("Owner")
     @State private var group = UserClass("Group")
-    @State private var everyone = UserClass("Everyone")
+    @State private var everyone = UserClass("Public")
     
-    var body: some View {
-        VStack {
-            let _ = print(owner)
-            HStack {
-                UserClassView(userClass: $owner)
-                UserClassView(userClass: $group)
-                UserClassView(userClass: $everyone)
-            }
+    func makeOctal(_ o: Settings, _ g: Settings, _ e: Settings) -> String {
+        return [o,g,e].reduce("") {
+            $0 + String(($1.read ? 4 : 0) + ($1.write ? 2 : 0) + ($1.execute ? 1 : 0))
+        }
+    }
+
+    func makeSymbol(_ o: Settings, _ g: Settings, _ e: Settings) -> String {
+        return [o,g,e].reduce("") {
+            $0 + ($1.read ? "r" : "-") + ($1.write ? "w" : "-") + ($1.execute ? "x" : "-")
+        }
+    }
+    
+    var Main: some View {
+        VStack() {
             
             HStack {
                 Text(makeOctal(owner.settings, group.settings, everyone.settings))
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color(hue: 1.0, saturation: 0.028, brightness: 0.392, opacity: 0.549))
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
                 Text(makeSymbol(owner.settings, group.settings, everyone.settings))
+                    .frame(maxWidth: .infinity)
+                    .padding()
+                    .background(Color(hue: 1.0, saturation: 0.028, brightness: 0.392, opacity: 0.549))
+                    .cornerRadius(10)
+                    .foregroundColor(.white)
             }
+            Spacer()
+            HStack() {
+                UserClassView(userClass: $owner)
+                UserClassView(userClass: $group)
+                UserClassView(userClass: $everyone)
+            }.padding(.bottom)
+        }.padding()
+    }
+    
+    var body: some View {
+        NavigationView {
+            Main.navigationBarTitle(Text("chmody"))
         }
     }
 }
